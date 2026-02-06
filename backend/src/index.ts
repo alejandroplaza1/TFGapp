38251ea6cce261e0823a6f3ab0be21402560b544
aly-tnd <alycheikhnatandia@gmail.com>
2026-02-06 18:54:47 +0100

    Añadir configuración de MongoDB: database.ts, actualizar docker-compose, index.ts y scripts de build

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import { connectDB } from './config/database';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Conectar a MongoDB
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('¡Hola desde el contenedor Docker del Backend!');
